257c22cab69dcf0effdb6a60c30d83d4eae47932
A Venkatesh <avenkateshanand1998@gmail.com>
2024-02-01 16:27:57 +0530

    this project was Expanding the cards using angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Expanding_Cards';
+  panels!: NodeListOf<Element>;
+
+  ngOnInit() {
+    // This is executed after the view has been initialized
+    this.panels = document.querySelectorAll('.panel');
+
+    this.panels.forEach((panel: Element) => {
+      panel.addEventListener('click', () => {
+        this.removeActiveClasses();
+        panel.classList.add('active');
+      });
+    });
+  }
+
+  removeActiveClasses() {
+    this.panels.forEach((panel: Element) => {
+      panel.classList.remove('active');
+    });
+  }
 }
